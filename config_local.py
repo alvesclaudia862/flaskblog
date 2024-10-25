297aa8dc9c317d0db9109e9e4015bfbb931a7426
Claudia Alves <alvesclaudia862@gmail.com>
2024-10-25 11:55:24 -0300

    feat.2024.10.25

---
+++ config_local.py
@@ -0,0 +1,5 @@
+# config_local.py
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = ''
+MYSQL_DB = 'flaskblogdb'
---
+++ config_production.py
@@ -0,0 +1,5 @@
+# Conigurações do MySQL para produção (PythonAnyWhere)
+MYSQL_HOST = 'claudiasouza.mysql.pythonanywhere-services.com'
+MYSQL_USER = 'claudiasouza'
+MYSQL_PASSWORD = '*Erick3006'
+MYSQL_DB = 'claudiasouza$default'
